5530eea4b782f6e3853d77ff382df51eccb0259d
Jens Haulund <jenshaulund@Jenss-iMac.frontier.com>
2015-12-08 11:09:44 -0500

    After fix to css

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-aslkdfj
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="_/j/bootstrap.js"></script>
+  </body>
+</html>
